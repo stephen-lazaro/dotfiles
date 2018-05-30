18e3bc15d37a6796d0a5caa27be4d58a2b11da9e
Stephen Lazaro <stephen.lazaro@gmail.com>
2018-05-29 18:54:47 -0700

    Updates and add some dumb theorem provers
    Like the pretentious asshole I am

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ curl https://sh.rustup.rs -sSf | sh
 rustup default nightly
 
 # Install haskell
-brew cask install haskell-platform
+brew install ghc cabal-install stack
 cabal update
 
 # Install Elixir
@@ -46,6 +46,13 @@ cargo install cargo-wasm  # Cargo support for wasm compilation
 cargo install pijul       # darcs like
 cargo install bat         # Colorized awesome cat replacement
 cabal install ShellCheck  # Syntax checking for bash!
+cabal install Agda        # Agda proof assistant
+
+# Setup agda
+agda-mode setup
+
+# Setup Coq
+echo "Go install CoQ manually because I guess that's what's up you dumb asshole"
 
 # Obtain Carp
 git clone git@github.com:carp-lang/Carp.git
